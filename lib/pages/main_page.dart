ffa09e2647a41646c870e4c0025eeab5925ae064
aprxty3 <ajiprxty3@gmail.com>
2022-01-13 15:21:57 +0700

    build homepage menu

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 
 class MainPage extends StatelessWidget {
-  const MainPage({ Key? key }) : super(key: key);
+  final zoomDrawerController;
 
+  MainPage(this.zoomDrawerController);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Home Page'),
+        leading: InkWell(
+          onTap: () => zoomDrawerController.toggle(),
+          child: Icon(Icons.menu),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [],
+        ),
+      ),
     );
   }
 }
