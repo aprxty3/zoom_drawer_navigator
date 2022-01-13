7e8a2584042bedf61a019e6c02b72d11884dc73f
aprxty3 <ajiprxty3@gmail.com>
2022-01-13 14:24:43 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Just learn how to make a zoom in/out drawer
---
+++ android/app/src/main/kotlin/com/example/zoom_drawer_navigator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zoom_drawer_navigator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
